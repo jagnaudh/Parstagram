bb43f7e949cab2bfc1f592c8e7dc87bd58d07a5b
j.bhatia <j.bhatia@h6s-mac06f.local>
2019-03-21 18:08:10 -0400

    new features

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Gangstagram"
+                configuration.server = "https://fathomless-citadel-11617.herokuapp.com/parse"
+            })
+        )
+        
         return true
     }
 
